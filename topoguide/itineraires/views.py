32aae130d8daf97c68663885257cd363cbe95aa5
Matthieu BASSET <bassetmatt@gmail.com>
2022-04-20 00:14:27 +0200

    Creating a first view for itineraires

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from .models import Itineraire
+from django.template import loader
 
-# Create your views here.
+
+def itineraires(request):
+    route_list = Itineraire.objects.order_by('title')
+    context = {
+        'route_list' : route_list   
+    }
+    return render(request, 'itineraires/itineraires.html', context )
