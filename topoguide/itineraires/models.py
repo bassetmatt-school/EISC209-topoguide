6e1d5ad1ec07bbb9d0e81ed6f61ea185c4ee709e
matt <bassetmatt@gmail.com>
2022-04-21 09:37:48 +0200

    Add model for sortie

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,6 +1,8 @@
-from django.db import models
 from datetime import timedelta
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
 
 class Itineraire(models.Model) :
     #Main info
@@ -20,10 +22,54 @@ class Itineraire(models.Model) :
     #Using a timedelta object for intuitive fields for duration
     estim_duration = models.DurationField(default=timedelta(hours=1))
     #Ensures the difficulty is between 1 and 5
-    estim_difficulty = models.IntegerField(default=1, validators=[
+    estim_difficulty = models.IntegerField(
+        default=1, 
+        validators=[
             MaxValueValidator(5),
             MinValueValidator(1)
         ])
     
     def __str__(self) :
         return self.title
+
+class Sortie(models.Model) :
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    route = models.ForeignKey(Itineraire, on_delete=models.CASCADE)
+    date = models.DateField(timezone.now().date())
+    actual_duration = models.DurationField(default=timedelta(hours=1))
+    number_people = models.IntegerField(default=1)
+    
+    XP_LEVEL = [
+        ('B', "Beginners"),
+        ('E', "Experienced"),
+        ('M', "Mixed"),
+    ]
+    
+    group_xp = models.CharField(
+        choices = XP_LEVEL,
+        max_length = 1,
+        default = 'B'
+    )
+    
+    WEATHER_TYPES = [
+        ('G', "Good"),
+        ('N', "Neutral"),
+        ('B', "Bad"),
+    ]
+    
+    weather = models.CharField(
+        choices = WEATHER_TYPES,
+        max_length = 1,
+        default = 'N'
+    )
+    
+    #Ensures the difficulty is between 1 and 5
+    difficulty_felt = models.IntegerField(
+        default = 1, 
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ])
+
+    def __str__(self) :
+        return f"{self.route} by {self.user}"
