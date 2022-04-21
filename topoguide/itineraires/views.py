5ab78e2242e9fd6faf2d1bd7eca6fe8bde13ede9
matt <bassetmatt@gmail.com>
2022-04-21 09:40:38 +0200

    Add a basic view for sorties list

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404, render
 from django.views import generic
-from .models import Itineraire
+from .models import Itineraire, Sortie
 
 class ItinerairesView(generic.ListView):
     template_name = 'itineraires/itineraires.html'
@@ -7,3 +8,15 @@ class ItinerairesView(generic.ListView):
     
     def get_queryset(self):
         return Itineraire.objects.order_by('title')
+
+def sorties(request, itineraire_id):
+    itineraire = get_object_or_404(Itineraire,pk=itineraire_id)
+    return render(request,
+                  'itineraires/sorties.html',
+                  {'itineraire_id':itineraire.id})
+
+"""
+class SortiesView(generic.DetailView):
+    model = Itineraire
+    album = get_object_or_404(Sortie, pk=model.id)
+""" 
